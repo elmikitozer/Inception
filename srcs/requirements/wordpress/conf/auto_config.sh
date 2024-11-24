834377b86c7100d99db14de8de478fb45f4291af
Mikaya Yevou <myevou@made-f0Dr9s19.clusters.42paris.fr>
2024-11-24 16:42:53 +0100

    done w/ dockerfiles

---
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -0,0 +1,27 @@
+#wait for mariadb to be up
+sleep 10
+
+
+#install wordpress
+if [ ! -f /var/www/html/wp-config.php ]; then
+	wp config create --allow-root \
+		--dbname=$SQL_NAME \
+		--dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD \
+		--dbhost=mariadb:3306 --path='var/www/worpress'
+
+	wp core install --url=$domain_name \
+		--title=$brand\
+		--admin_user=$wordpress_admin \
+		--admin_password=$wordpress_admin_password \
+		--admin_email=$wordpress_admin_email \
+		--allow-root
+
+	wp user create $login $wp_user_email\
+		--role=author \
+		--user_pass=$wp_user_pwd\
+		--allow-root
+
+fi
+
+usr/sbin/php-fpm7.3 -F
